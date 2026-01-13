dbdc22ae6fec5a2efd328a9a8a55ff1954f51f3b
Trong Huy <huytrongk8a@gmail.com>
2026-01-13 11:20:39 +0700

    test structure

---
+++ ams_be/src/main/java/org/example/ams_be/AmsBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.ams_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmsBeApplication.class, args);
+    }
+
+}
---
+++ ams_be/src/test/java/org/example/ams_be/AmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ams_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
