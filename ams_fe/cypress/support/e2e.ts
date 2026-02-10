ca6ae11a283325747eb8d60792cab4aac107d992
Tranquangkhai12 <tranquangkhai307@gmail.com>
2026-02-10 13:22:25 +0700

    xóa toàn bộ file FE cũ

--- ams_fe/cypress/support/e2e.ts
+++
@@ -1,9 +0,0 @@
-// ***********************************************************
-// This file is processed and loaded automatically before your test files.
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
-import './commands';
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
