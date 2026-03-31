28b37081d62094ba0d3106d7dd5d701c4688f5aa
Tranquangkhai12 <tranquangkhai307@gmail.com>
2026-03-31 20:27:06 +0700

    feat: implement initial UI components and layout structure for core system modules

--- ams_fe/src/app/app.module.ts
+++ ams_fe/src/app/app.module.ts
@@ -9,10 +9,20 @@ import { LoginComponent } from './features/auth/login/login.component';
 import { AdminLayoutComponent } from './core/layout/admin-layout/admin-layout.component';
 import { AiChatbotComponent } from './core/layout/ai-chatbot/ai-chatbot.component';
 import { ProfileComponent } from './features/admin/profile/profile.component';
+import { NgIconsModule } from '@ng-icons/core';
+import * as heroiconsOutline from '@ng-icons/heroicons/outline';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, AdminLayoutComponent, AiChatbotComponent, ProfileComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, CoreModule, SharedModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    // Register all Heroicons Outline ids globally so <ng-icon> always has an id to render.
+    NgIconsModule.withIcons(heroiconsOutline as unknown as Record<string, string>),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
