d017110de320cea16a60c27edd464c5fca12bf29
Tranquangkhai12 <tranquangkhai307@gmail.com>
2026-06-25 21:35:21 +0700

    khai

--- ams_fe/cypress/support/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Cypress {
-  interface Chainable {
-    login(): Chainable<void>;
-    waitForOverlayToDisappear(): Chainable<void>;
-  }
-}
--- ams_fe/src/app/features/admin/batch-processing/batch-processing.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: false,
-  selector: 'app-batch-processing',
-  templateUrl: './batch-processing.component.html',
-  styleUrls: ['./batch-processing.component.scss'],
-})
-export class BatchProcessingComponent {}
