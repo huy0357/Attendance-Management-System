89d38ef210384b5cde492401342f0308751c0d3e
Trong Huy <huytrongk8a@gmail.com>
2026-07-07 21:05:47 +0700

    fix: polyfill crypto.randomUUID for non-HTTPS environment

--- ams_fe_react/src/main.tsx
+++ ams_fe_react/src/main.tsx
@@ -4,6 +4,23 @@ import App from './app/App';
 import './styles/tokens.scss';
 import './styles/global.scss';
 
+// Polyfill crypto.randomUUID cho môi trường HTTP (chưa có secure context)
+// TODO: Xoá đoạn này sau khi cấu hình HTTPS xong cho domain
+if (!window.crypto) {
+  // @ts-ignore
+  window.crypto = {};
+}
+if (typeof window.crypto.randomUUID !== 'function') {
+  // @ts-ignore
+  window.crypto.randomUUID = function (): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      const r = (Math.random() * 16) | 0;
+      const v = c === 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  };
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
