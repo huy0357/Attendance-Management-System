ef4d261363702009c0d7393b4c612942d3040c14
Trong Huy <huytrongk8a@gmail.com>
2026-07-08 21:49:06 +0700

    fix: remove wrong Node.js polyfill package, use custom browser polyfill for crypto.randomUUID

--- ams_fe_react/src/main.tsx
+++ ams_fe_react/src/main.tsx
@@ -1,11 +1,18 @@
-// ====================== POLYFILL SIÊU SỚM ======================
-// @ts-ignore
-import randomUUID from 'crypto-randomuuid';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './styles/tokens.scss';
 import './styles/global.scss';
+
+// Polyfill crypto.randomUUID cho môi trường HTTP (chưa có secure context)
+// TODO: Xoá đoạn này sau khi cấu hình HTTPS xong cho domain
+function polyfillRandomUUID(): string {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
 if (typeof window.crypto === 'undefined') {
   // @ts-ignore
@@ -13,7 +20,7 @@ if (typeof window.crypto === 'undefined') {
 }
 if (typeof window.crypto.randomUUID !== 'function') {
   Object.defineProperty(window.crypto, 'randomUUID', {
-    value: randomUUID,
+    value: polyfillRandomUUID,
     writable: true,
     configurable: true,
   });
